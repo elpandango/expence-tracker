dd75b48fef16712c378a01bb93b68dd36ce68e4f
Eugen Radchenko <elpandango@gmail.com>
2025-01-10 15:37:57 +0100

    Trying to add PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   ssr: true,
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   nitro: {
     plugins: ["~/server/plugins/mongodb.ts"],
   },
@@ -29,9 +29,34 @@ export default defineNuxtConfig({
       },
       'Material+Symbols+Outlined': true,
     }
-  }], '@pinia/nuxt'],
+  }], '@pinia/nuxt', '@vite-pwa/nuxt'],
   plugins: ['~/plugins/router.ts'],
   server: {
     plugins: ['~/server/plugins/initializeCategories.ts'],
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Expendango - Expense Tracking App',
+      short_name: 'Expendango',
+      description: 'Expendango – your personal assistant in the world of finance! Manage your money effortlessly: track expenses, analyze income, and plan for the future. Financial freedom starts here.',
+      lang: 'en',
+      display: 'standalone',
+      start_url: '/',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
   },
 })
