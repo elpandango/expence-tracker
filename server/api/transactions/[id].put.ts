aa63e3c40261ffc339e63b1e59766607af6fc42c
Eugen Radchenko <elpandango@gmail.com>
2025-01-16 16:45:42 +0100

    Controllers cleanup.

--- server/api/transactions/[id].put.ts
+++ server/api/transactions/[id].put.ts
@@ -1,81 +1,27 @@
-import mongoose from "mongoose";
-import { TransactionModel } from "~/server/models/TransactionModel";
-import { AccountModel } from "~/server/models/AccountModel";
-import { getCookie } from "h3";
+import {getCookie} from "h3";
+import {updateTransaction} from "~/server/controllers/transactionController";
 
 export default defineEventHandler(async (event) => {
-  const { id } = event.context.params;
+  const {id} = event.context.params;
   const userId = getCookie(event, "userId");
   const updatedTransaction = await readBody(event);
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    throw createError({ statusCode: 400, message: "Invalid transaction ID format" });
-  }
+  try {
+    const newTransaction = await updateTransaction(updatedTransaction, id, userId);
 
-  const transactionId = new mongoose.Types.ObjectId(id);
-  const oldTransaction = await TransactionModel.findOne({ _id: transactionId, userId });
-
-  if (!oldTransaction) {
-    throw createError({ statusCode: 404, message: "Transaction not found" });
-  }
-
-  const account = await AccountModel.findOne({ _id: updatedTransaction.accountId, userId });
-  if (!account) {
-    throw createError({ statusCode: 404, message: "Account not found" });
-  }
-
-  if (account.currency !== updatedTransaction.currency) {
-    throw createError({
-      statusCode: 400,
-      message: `Currency mismatch: Account currency (${account.currency}) does not match transaction currency (${updatedTransaction.currency}).`,
-    });
-  }
-
-  const isAmountChanged = oldTransaction.amount !== updatedTransaction.amount;
-  const isAccountChanged = oldTransaction.accountId.toString() !== updatedTransaction.accountId;
-  const isTypeChanged = oldTransaction.type !== updatedTransaction.type;
-  const formatDate = (date: string) => new Date(date).toISOString().split('T')[0];
-  const isDateChanged = formatDate(oldTransaction.date) !== formatDate(updatedTransaction.date);
-
-  if (isAmountChanged || isAccountChanged || isTypeChanged || isDateChanged) {
-    const oldAccount = await AccountModel.findById(oldTransaction.accountId);
-    if (oldAccount) {
-      if (oldTransaction.type === "expense") {
-        oldAccount.balance += oldTransaction.amount;
-      } else if (oldTransaction.type === "income") {
-        oldAccount.balance -= oldTransaction.amount;
+    if (newTransaction) {
+      return {
+        status: 200,
+        message: "Transaction updated successfully",
+        transaction: newTransaction,
       }
-      await oldAccount.save();
+    } else {
+      return {
+        status: 400,
+        message: "No changes detected",
+      };
     }
-
-    await TransactionModel.findByIdAndDelete(transactionId);
-
-    const newTransaction = new TransactionModel({
-      ...updatedTransaction,
-      userId,
-    });
-
-    const updatedAccount = await AccountModel.findById(updatedTransaction.accountId);
-    if (updatedAccount) {
-      if (updatedTransaction.type === "expense") {
-        updatedAccount.balance -= updatedTransaction.amount;
-      } else if (updatedTransaction.type === "income") {
-        updatedAccount.balance += updatedTransaction.amount;
-      }
-      await updatedAccount.save();
-    }
-
-    await newTransaction.save();
-
-    return {
-      status: 200,
-      message: "Transaction updated successfully",
-      transaction: newTransaction,
-    };
-  } else {
-    return {
-      status: 400,
-      message: "No changes detected",
-    };
+  } catch (err) {
+    throw err;
   }
 });
--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import { UserModel } from "~/server/models/UserModel";
+import { createError } from 'h3';
+
+interface AuthLoginParams {
+  email: string;
+  password: string;
+}
 
 interface AuthRegisterParams {
   name: string;
@@ -15,6 +21,32 @@ interface AuthRegisterResponse {
   userId?: string;
   message?: string;
 }
+
+export const authLogin = async ({ email, password }: AuthLoginParams) => {
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    throw createError({ statusCode: 404, message: 'A user with this email could not be found.' });
+  }
+
+  const isEqual = await bcrypt.compare(password, user.password);
+  if (!isEqual) {
+    throw createError({ statusCode: 401, message: 'Wrong password!' });
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      userId: user._id.toString(),
+    },
+    'somesupersecretsecret',
+    { expiresIn: '1y' }
+  );
+
+  return {
+    token,
+    userId: user._id.toString(),
+  };
+};
 
 export const authRegister = async ({ name, lastName, email, password }: AuthRegisterParams): Promise<AuthRegisterResponse> => {
   try {
