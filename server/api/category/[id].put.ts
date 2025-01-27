bef2e0cfe6d52962b9386889af77256e63a9cb0e
Eugen Radchenko <elpandango@gmail.com>
2025-01-27 12:13:34 +0100

    Working on API testing: All 'Category' APIs tested.

--- server/api/category/[id].put.ts
+++ server/api/category/[id].put.ts
@@ -1,8 +1,18 @@
 import { CategoryModel } from '~/server/models/CategoryModel';
+import {createError, getCookie, defineEventHandler, readBody} from "h3";
 
 export default defineEventHandler(async (event) => {
   const { id } = event.context.params;
   const body = await readBody(event);
+
+  const userId = getCookie(event, 'userId');
+  if (!userId) {
+    throw createError({ statusCode: 401, message: 'Unauthorized' });
+  }
+
+  if (!body.name || typeof body.name !== 'string' || body.name.trim() === '') {
+    throw createError({ statusCode: 400, message: 'Category name is required and must be a string' });
+  }
 
   const updatedCategory = await CategoryModel.findByIdAndUpdate(id, {
     name: body.name,
--- server/api/category/index.get.ts
+++ server/api/category/index.get.ts
@@ -1,6 +1,21 @@
+import {createError, getCookie, defineEventHandler} from 'h3';
 import {CategoryModel} from '~/server/models/CategoryModel';
 
 export default defineEventHandler(async (event) => {
-  const categories = await CategoryModel.find().select('name icon color _id');
-  return {status: 200,  message: 'Expense categories retrieved successfully', categories};
+  try {
+    const userId = getCookie(event, 'userId');
+    if (!userId) {
+      throw createError({statusCode: 401, message: 'Unauthorized'});
+    }
+
+    const categories = await CategoryModel.find();
+    return {status: 200, message: 'Expense categories retrieved successfully', categories};
+  } catch (error) {
+    console.error(error);
+
+    throw createError({
+      statusCode: error.statusCode || 500,
+      message: error.message || 'An unexpected error occurred',
+    });
+  }
 });
