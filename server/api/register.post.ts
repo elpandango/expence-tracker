3fcc81122be3da55e102ba3ea01fcb3a5614512d
Eugen Radchenko <elpandango@gmail.com>
2025-02-14 08:52:44 +0100

    Some fixes in project

--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,38 +1,43 @@
 import {authRegister} from "~/server/controllers/authController/authRegister";
-import { readBody, setCookie } from 'h3';
+import {readBody, setCookie} from 'h3';
 
-export default defineEventHandler(async (event) => {
-    const {name, lastName, email, password} = await readBody(event);
-    const cookieAge = 60 * 60 * 24 * 7;
+interface RegisterResponse {
+  status: number;
+  userId: string;
+}
 
-    try {
-        const { token, userId } = await authRegister({ name, lastName, email, password });
+export default defineEventHandler(async (event): Promise<RegisterResponse> => {
+  const {name, lastName, email, password} = await readBody(event);
+  const cookieAge = 60 * 60 * 24 * 7;
 
-        setCookie(event, 'auth_token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: cookieAge,
-        });
+  try {
+    const {token, userId} = await authRegister({name, lastName, email, password});
 
-        setCookie(event, 'userId', userId, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
-            path: '/',
-            maxAge: cookieAge,
-        });
+    setCookie(event, 'auth_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: cookieAge,
+    });
 
-        setCookie(event, 'isAuthenticated', true, {
-            httpOnly: false,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: cookieAge,
-        });
+    setCookie(event, 'userId', userId, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+      maxAge: cookieAge,
+    });
 
-        return { status: 200, userId };
-    } catch (err) {
-        console.error("Error during register process: ", err);
-        throw err;
-    }
+    setCookie(event, 'isAuthenticated', 'true', {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: cookieAge,
+    });
+
+    return {status: 200, userId};
+  } catch (err) {
+    console.error("Error during register process: ", err);
+    throw err;
+  }
 });
